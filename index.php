206f5944e5c99a04856a2056006cb5e55221cbfd
Pascal311 <griffonpascalpro@gmail.com>
2024-12-20 15:27:05 +0100

    composer

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+// Afficher toutes les erreurs PHP pour le débogage
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Chemin vers les dépendances installées via Composer
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Chargement manuel de Yaml.php si non géré par Composer (ajuste le chemin si nécessaire)
+if (!class_exists('Symfony\Component\Yaml\Yaml')) {
+    require_once __DIR__ . 'vendor/symfony/yaml/Yaml.php';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,10 +21,6 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/portfolio/vendor/symfony/yaml/Yaml.php");
-?>
 
 <header>
     <h1>Mon Portfolio</h1>
@@ -23,12 +33,14 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/portfolio/vendor/symfony/yaml/Yaml.ph
     </nav>
 </header>
 
-
-<?php include 'pages/accueil.php'; ?>
-<?php include 'pages/competences.php'; ?>
-<?php include 'pages/formation.php'; ?>
-<?php include 'pages/realisations.php'; ?>
-<?php include 'pages/contact.php'; ?>
+<?php
+// Inclusion des sections de la page
+include 'pages/accueil.php';
+include 'pages/competences.php';
+include 'pages/formation.php';
+include 'pages/realisations.php';
+include 'pages/contact.php';
+?>
 
 <footer>
     <p>&copy; <?php echo date("Y"); ?> Mon Portfolio</p>
