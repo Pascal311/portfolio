1cd6e80515a90c6862593ef920ad800609187745
Pascal311 <griffonpascalpro@gmail.com>
2024-12-20 16:11:59 +0100

    autoload + yaml parse file

--- pages/realisations.php
+++ pages/realisations.php
@@ -1,10 +1,19 @@
 <?php
+// Afficher toutes les erreurs PHP pour le débogage
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-require_once($_SERVER['DOCUMENT_ROOT'] . "/portfolio/vendor/symfony/yaml/Yaml.php");
+// Chemin vers les dépendances installées via Composer
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Chargement manuel de Yaml.php si non géré par Composer (ajuste le chemin si nécessaire)
+if (!class_exists('Symfony\Component\Yaml\Yaml')) {
+    require_once __DIR__ . 'vendor/symfony/yaml/Yaml.php';
+}
 
 // Charger le fichier YAML avec le chemin absolu
 $data = yaml_parse_file($_SERVER['DOCUMENT_ROOT'] . "/portfolio/data/realisations.yaml");
-?>
+
 ?>
 
 <section id='realisations'>
